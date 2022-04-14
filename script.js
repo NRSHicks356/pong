850f2ed7fe2c1289dde71a2b862c70c6a8a0fde7
NRSHicks356 <85139050+NRSHicks356@users.noreply.github.com>
2022-04-14 14:09:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
 const buttons = document.querySelectorAll("button");
 const h1 = document.querySelector("h1");
 const score = document.querySelector("#score");
+const span1 = document.querySelector("#p1Score");
+const span2 = document.querySelector("#p2Score");
+const button1 = document.querySelector("#player1Button");
+const button2 = document.querySelector("#player2Button");
+const reset = document.querySelector("#resetButton");
 let count1 = 0;
 let count2 = 0;
-let numbers = score.options[score.selectedIndex].text;
-buttons[0].addEventListener("click", function(e) {
-    let numbers = score.options[score.selectedIndex].text;
-
-    if (count1 < numbers) {
+let gameOver = false;
+let numbers = () => parseInt(score.options[score.selectedIndex].text);
+button1.addEventListener("click", function(e) {
+    numbers();
+    if (!gameOver) {
         count1++;
+    } else {
+        button1.disabled = true;
+        button2.disabled = true;
     }
-    h1.innerText = `${count1} to ${count2}`;
-});
 
-buttons[1].addEventListener("click", function(e) {
-    if (count2 < numbers) {
+    if (count1 === numbers()) {
+        button1.disabled = true;
+        button2.disabled = true;
+        span1.classList.add("winner");
+        span2.classList.add("loser");
+    }
+    span1.innerText = count1;
+});
+button2.addEventListener("click", function(e) {
+    numbers();
+    if (!gameOver) {
         count2++;
+    } else {
+        button1.disabled = true;
+        button2.disabled = true;
     }
-    h1.innerText = `${count1} to ${count2}`;
+
+    if (count2 === numbers()) {
+        button1.disabled = true;
+        button2.disabled = true;
+        span2.classList.add("winner");
+        span1.classList.add("loser");
+    }
+    span2.innerText = count2;
 });
 
-buttons[2].addEventListener("click", function(e) {
+reset.addEventListener("click", function(e) {
+    numbers();
     count1 = 0;
     count2 = 0;
-    numbers = score.options[score.selectedIndex].text;
-    h1.innerText = `${count1} to ${count2}`;
+    span1.innerText = count1;
+    span2.innerText = count2;
+    span2.classList.remove("winner");
+    span1.classList.remove("winner");
+    span2.classList.remove("loser");
+    span1.classList.remove("loser");
+    button1.disabled = false;
+    button2.disabled = false;
 });
